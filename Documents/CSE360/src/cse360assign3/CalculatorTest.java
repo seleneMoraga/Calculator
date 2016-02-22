14a52e0b4dc40c3bcd4792f7377cd4f8dc9dd623
smoraga <smoraga@EN4110023214.fulton.ad.asu.edu>
2016-02-22 11:29:17 -0700

    Initial version of caluculator

---
+++ Documents/CSE360/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,44 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		assertNotNull("Not yet implemented");
+	}
+
+	@Test
+	public void getTotalTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void addTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void subtractTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void multiplyTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void divideTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void getHistoryTest() {
+		fail("Not yet implemented");
+	}
+
+}
